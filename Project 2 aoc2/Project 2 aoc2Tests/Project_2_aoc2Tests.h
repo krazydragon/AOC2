b125407eaa8f1e4778fed5b45705532ec591a0a2
Ronaldo Barnes <krazydragon@fullsail.edu>
2012-07-09 20:28:21 -0700

    project 2 started

---
+++ Project 2 aoc2/Project 2 aoc2Tests/Project_2_aoc2Tests.h
@@ -0,0 +1,13 @@
+//
+//  Project_2_aoc2Tests.h
+//  Project 2 aoc2Tests
+//
+//  Created by Ronaldo Barnes on 7/9/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Project_2_aoc2Tests : SenTestCase
+
+@end
---
+++ Project 2 aoc2/Project 2 aoc2Tests/Project_2_aoc2Tests.m
@@ -0,0 +1,32 @@
+//
+//  Project_2_aoc2Tests.m
+//  Project 2 aoc2Tests
+//
+//  Created by Ronaldo Barnes on 7/9/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import "Project_2_aoc2Tests.h"
+
+@implementation Project_2_aoc2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Project 2 aoc2Tests");
+}
+
+@end
